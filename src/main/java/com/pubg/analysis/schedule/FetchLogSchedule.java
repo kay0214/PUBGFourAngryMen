bc0e1fffec7bafe74105482cf44072f6ad882b2e
sunpeikai <spk123456>
2020-07-16 10:50:37 +0800

    1、处理一下matchLogId,虽然没什么用处

--- src/main/java/com/pubg/analysis/schedule/FetchLogSchedule.java
+++ src/main/java/com/pubg/analysis/schedule/FetchLogSchedule.java
@@ -13,9 +13,11 @@ import com.pubg.analysis.utils.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
@@ -34,6 +36,7 @@ public class FetchLogSchedule {
 
     @Scheduled(cron = "0 0/1 * * * ?")
     public void fetchLogRemote(){
+        long startTime = System.currentTimeMillis();
         List<Match> matches = matchRepository.findMatchToFetch();
         log.info("本次拉取对局日志start,count[{}]",matches.size());
         for(Match match : matches){
@@ -42,21 +45,31 @@ public class FetchLogSchedule {
             log.info("拉取对局日志,matchId[{}],url[{}]",matchId,url);
             String result = HttpUtil.sendGetGZIP(url);
             JSONArray array = JSONObject.parseArray(result);
+            AtomicReference<String> matchLogId = new AtomicReference<>();
             List<BaseLog> baseLogs = array
                     .parallelStream()
                     .map(e -> {
                         JSONObject json = (JSONObject) e;
                         BaseLog baseLog = json.toJavaObject(BaseLog.class);
-                        baseLog.setMatchLogId(baseLog.getMatchId());
+                        if(!StringUtils.isEmpty(baseLog.getMatchLogId())){
+                            matchLogId.set(baseLog.getMatchId());
+                        }
                         baseLog.setMatchId(matchId);
                         return baseLog;
                     })
                     .collect(Collectors.toList());
+
+            // 填充matchLogId - 其实matchLogId没什么用
+            if(!StringUtils.isEmpty(matchLogId.get())){
+                baseLogs.forEach(baseLog -> {
+                    baseLog.setMatchLogId(matchLogId.get());
+                });
+            }
             // 插入对局日志
             logRepository.insertAll(baseLogs);
             // 更新状态
             matchRepository.updateFetchLogStatus(matchId);
         }
-        log.info("本次拉取对局日志end,count[{}]",matches.size());
+        log.info("本次拉取对局日志end,count[{}],耗时[{}]",matches.size(),(System.currentTimeMillis() - startTime)/1000 + "s");
     }
 }
